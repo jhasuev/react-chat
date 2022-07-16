539c81250628fa389892706f37eda22a62a20e04
Jamal Hasuev <metafra@mail.ru>
2022-07-17 01:49:03 +0300

    add icons + add footer layout (message enter form)

--- src/components/chat-footer.jsx
+++ src/components/chat-footer.jsx
@@ -1,7 +1,29 @@
+import Paper from '@mui/material/Paper'
+import InputBase from '@mui/material/InputBase'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import Send from '@mui/icons-material/Send'
+
 export default function ChatFooter() {
   return (
-    <div>
-      ChatFooter
-    </div>
+    <Paper
+      component="form"
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <InputBase
+        multiline
+        maxRows={3}
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Message"
+      />
+      <Divider sx={{  m: 0.5 }} orientation="vertical" />
+      <IconButton type="submit" sx={{ p: '10px' }}>
+        <Send fontSize="large" />
+      </IconButton>
+    </Paper>
   )
 }
